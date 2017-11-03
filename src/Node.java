7850b7cfeba043503f92e0f1e9b46eaf1ca555c5
Jonathan Sigel <jsigel@wpi.edu>
2017-11-02 22:40:20 -0400

    More comments, minor method modifications.

--- src/Node.java
+++ src/Node.java
@@ -10,6 +10,9 @@ public class Node<T, U> {
         fNext = next;
     }
 
+    /**
+     * Removes a node from the linked list by setting the fPrev and fNext of the nodes in front fo and behind it.
+     */
     public void remove() {
         if(fPrev != null)
             fPrev.setNext(fNext);
@@ -17,17 +20,34 @@ public class Node<T, U> {
             fNext.setPrev(fPrev);
     }
 
+    /**
+     * Changes the node previous to the node that setPrev is called upon.
+     * @param newNode the node to set as the previous node.
+     */
     public void setPrev(Node newNode) {
         fPrev = newNode;
     }
-
+    /**
+     * Changes the node ahead of the node that setNext is called upon.
+     * @param newNode the node to set as the previous node.
+     */
     public void setNext(Node newNode) {
         fNext = newNode;
     }
 
+    /**
+     * Gets the element associated with a node.
+     * @return fElement, the element associated with a node.
+     */
     public U getElement() {
         return fElement;
     }
+
+
+    /**
+     * Gets the next node in the list, relative to the node it is called upon.
+     * @return fNext, the next node.
+     */
 
     public T getKey() {
         return fKey;
