c48b9124644b2e2848274fe939dd57d5c91cb22b
khincce <ksegenchuk@wpi.edu>
2017-11-07 22:26:04 -0500

    stuff

--- test/CacheTest.java
+++ test/CacheTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
  */
 public class CacheTest {
 
-	 private class StringLoader implements DataProvider<Integer, String> {
+	 public class StringLoader implements DataProvider<Integer, String> {
 		public StringLoader() {
 		}
 
